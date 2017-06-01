188b516cbde0a3a43d1e0634fad5864f7d4eaefd
saamybe <saminathan.ponnusami@voya.com>
2017-06-01 13:40:35 -0400

    Component LifeCycle and Data Binding

--- src/app.js
+++ src/app.js
@@ -1,3 +1,41 @@
 export class App {
-  message = 'Welcome to Aurelia!';
+
+
+    firstName = 'John';
+    lastName = 'Doe';
+    message = 'Welcome to Aurelia!';
+    i = 0;
+
+    constructor() {
+
+        console.log('constructor');
+
+    }
+
+    created() {
+        console.log('created');
+    }
+
+    bind() {
+        console.log('bind');
+    }
+
+    attached() {
+        console.log('attached');
+    }
+
+    detached() {
+        console.log('detached');
+    }
+
+    unbind() {
+        console.log('unbind');
+    }
+
+    sayHello() {
+        alert('Hello ' + this.firstName + ' ' + this.lastName + '. Nice to meet you.');
+        this.lastName = this.lastName + "" + this.i;
+        this.firstName = this.firstName + "" + this.i;
+        this.i = this.i + 1;
+    }
 }
