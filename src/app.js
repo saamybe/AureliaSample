a62bb664e15543f3e2ead5f7b5adcc9b6fe8ec06
Saamy <Saminathan.ponnusami@voya.com>
2017-06-13 23:39:52 -0400

    Aurelia Router Example

--- src/app.js
+++ src/app.js
@@ -1,3 +1,18 @@
 export class App {
   message = 'Welcome to Aurelia!';
+    configureRouter(config, router){
+        config.title = 'Aurelia';
+
+        config.map([
+            { route: '',  name: 'home',moduleId: './home',  nav: false, title:'home' },
+            { route: 'about',  name: 'about',moduleId: './about',    nav: true, title:'about' },
+            { route: 'home',  name: 'home',moduleId: './home',  nav: true, title:'home' }
+        ]);
+
+        this.router = router;
+    }
+    redirect(){
+        this.router.navigate("about");
+    }
+
 }
