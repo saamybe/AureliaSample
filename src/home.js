ddcb3f43d9103436f731a427d8fb24a50cdd084c
Saamy <Saminathan.ponnusami@voya.com>
2017-06-13 23:42:06 -0400

    Aurelia Router Example

--- src/home.js
+++ src/home.js
@@ -1,3 +1,15 @@
-export class Home{
+import {inject} from 'aurelia-framework';
+import {Router} from 'aurelia-router';
 
+
+@inject(Router)
+export class Home {
+
+    constructor(router){
+        this.router = router;
+    }
+
+    redirect(){
+        this.router.navigate("about");
+    }
 }
